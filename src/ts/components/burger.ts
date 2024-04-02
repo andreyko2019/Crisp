1d5b866aeee8f7dc32ac3d39125e0543d515c06c
3p-vln <vldknovak@gmail.com>
2024-04-02 18:04:43 +0300

    footer with adapt

--- src/ts/components/burger.ts
+++ src/ts/components/burger.ts
@@ -1,20 +1,31 @@
-import $ from 'jquery';
-
 export class Burger {
-  burgerBtn: JQuery<HTMLDivElement>;
-  menuAdapt: JQuery<HTMLDivElement>;
+  burgerBtn: HTMLElement;
+  menuAdapt: HTMLElement;
+  search: HTMLElement;
+  searchBtn: HTMLElement;
+
   constructor() {
-    this.burgerBtn = $('.burger-btn');
-    this.menuAdapt = $('.header__menu_adapt');
+    this.burgerBtn = document.querySelector('.burger-btn') as HTMLElement;
+    this.menuAdapt = document.querySelector('.header__menu_adapt') as HTMLElement;
+    this.search = document.querySelector('.header__search-and-bag .search') as HTMLElement;
+    this.searchBtn = document.querySelector('.header__search-and-bag .search svg') as HTMLElement;
 
     this.burger();
+    this.searchFunc();
   }
 
   burger(): void {
     const context = this;
-    this.burgerBtn.on('click', function () {
-      context.burgerBtn.toggleClass('active');
-      context.menuAdapt.toggleClass('active');
+    this.burgerBtn.addEventListener('click', function () {
+      context.burgerBtn.classList.toggle('active');
+      context.menuAdapt.classList.toggle('active');
+    });
+  }
+
+  searchFunc(): void {
+    const context = this;
+    this.searchBtn.addEventListener('click', function () {
+      context.search.classList.toggle('active');
     });
   }
 }
