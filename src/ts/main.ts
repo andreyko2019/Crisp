d2c1c12b799d274169dd3bf700edaab28583e041
3p-vln <vldknovak@gmail.com>
2024-05-02 19:19:31 +0300

    fix swiper

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,7 +2,7 @@ import { Burger } from './components/burger';
 import { Accordion } from './components/accordion';
 import { SummerSale } from './main/summer-sale-swiper';
 import { Loader } from './modules/stop-preload';
-import { Skeleton } from './components/skeleton';
+// import { Skeleton } from './components/skeleton';
 
 document.addEventListener('DOMContentLoaded', async () => {
   new Burger();
