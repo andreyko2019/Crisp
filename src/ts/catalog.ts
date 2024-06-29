d8fbd22d09d9619ae115c1dc7bd81112c3f4f6c1
Jagger899 <romanskarb2@ukr.net>
2024-06-29 12:03:34 +0300

    resolv confl

--- src/ts/catalog.ts
+++ src/ts/catalog.ts
@@ -1,19 +1,12 @@
 import { Burger } from './components/burger';
 import { Accordion } from './components/accordion';
-// import { Loader } from './modules/stop-preload';
-// import { dropdownWork } from './catalog/dropdown';
-// import { loadShowenBrands } from './catalog/accordeon';
 import { loadCards } from './catalog/loadcards';
 import { FilterAccordeon } from './catalog/accordeon';
-// import { rangeSliderWork } from './catalog/range-slider';
+
 
 document.addEventListener('DOMContentLoaded', async () => {
   new Burger();
   new Accordion();
-  // Loader.stop();
   new FilterAccordeon();
-  // dropdownWork();
   loadCards();
-  // loadShowenBrands();
-  // rangeSliderWork();
 });
